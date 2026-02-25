9f559cb7b391ce1eb3d1c406c5ea6264214a6ebd
poornima <poornima200828@gmail.com>
2026-02-25 20:54:51 +0530

    Modify heap creation and output for multiple arrays

--- make_heap.cpp
+++ make_heap.cpp
@@ -3,11 +3,20 @@
 using namespace std;
 
 int main() {
-    int arr[] = {4, 10, 3, 5, 1};
+    int arr1[] = {4, 10, 3, 5, 1};
     int n = 5;
 
-    make_heap(arr, arr+n);
+    make_heap(arr1, arr1+n);
 
     for(int i=0;i<n;i++)
-        cout<<arr[i]<<" ";
+        cout<<arr1[i]<<" ";
+
+
+    int arr2[] = {4, 10, 3, 5, 1};
+    int j = 5;
+
+    make_heap(arr2, arr2+j, greater<int>());
+
+    for(int i=0; i<j; i++)
+        cout << arr2[i] << " ";
 }
